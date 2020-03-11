8fa81e1b621fa5ed3328bddb96b499ae8096b0c8
Yuriy Martini <y.martini@soluzionesoftware.com>
2020-03-11 21:28:21 +0100

    Code optimizations

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -8,15 +8,22 @@ use Spatie\MediaLibrary\Models\Media as SpatieMedia;
 
 /**
  * @property string $collection_name
+ * @property string $url
  */
 class Media extends SpatieMedia
 {
     protected $hidden = ['model_type', 'model_id', 'disk', 'size', 'manipulations', 'created_at', 'updated_at'];
 
+    protected $appends = ['url'];
+
+    public function getUrlAttribute()
+    {
+        return $this->getUrl();
+    }
+
     public function toArray()
     {
         return array_merge(parent::toArray(), [
-            'url' => $this->getUrl(),
             'thumbnail' => $this->getUrl(),
         ]);
     }
