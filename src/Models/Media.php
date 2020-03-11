908de6d119c5e4fc3193ebeda95d123276d1af72
Yuriy Martini <y.martini@soluzionesoftware.com>
2020-03-11 21:27:49 +0100

    Hidden useless attributes from serialization.

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -11,6 +11,8 @@ use Spatie\MediaLibrary\Models\Media as SpatieMedia;
  */
 class Media extends SpatieMedia
 {
+    protected $hidden = ['model_type', 'model_id', 'disk', 'size', 'manipulations', 'created_at', 'updated_at'];
+
     public function toArray()
     {
         return array_merge(parent::toArray(), [
