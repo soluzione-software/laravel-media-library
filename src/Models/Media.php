ada7dcff43da80f51010d83bbed873762c4b0b86
Yuriy Martini <y.martini@soluzionesoftware.com>
2020-04-07 10:23:32 +0200

    Add conversions property to Media Model

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -4,21 +4,35 @@
 namespace SoluzioneSoftware\LaravelMediaLibrary\Models;
 
 
+use Illuminate\Support\Collection;
 use Spatie\MediaLibrary\Models\Media as SpatieMedia;
 
 /**
  * @property string $collection_name
  * @property string $url
+ * @property-read Collection conversions
  */
 class Media extends SpatieMedia
 {
     protected $hidden = ['model_type', 'model_id', 'disk', 'size', 'manipulations', 'created_at', 'updated_at'];
 
-    protected $appends = ['url'];
+    protected $appends = ['url', 'conversions'];
 
     public function getUrlAttribute()
     {
         return $this->getUrl();
+    }
+
+    public function getConversionsAttribute()
+    {
+        return $this
+            ->getGeneratedConversions()
+            ->filter(function ($value){
+                return $value;
+            })
+            ->map(function ($value, $key){
+                return $this->getUrl($key);
+            });
     }
 
     public function toArray()
